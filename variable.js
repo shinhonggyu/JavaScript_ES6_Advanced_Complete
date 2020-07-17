900be2af7586f14792555393c5b4694057d28c13
honggyu <zowoz0@naver.com>
2020-07-18 01:35:45 +0900

    데이터타입, data types, let vs var, hoisting

---
+++ variable.js
@@ -0,0 +1,98 @@
+// 1. Use strict
+// added in ES 5
+// use this for vanila javascript
+
+"use strict";
+
+// 2. Variable
+// let (added in ES6)
+let globalName = "global name";
+{
+  let name = "ellie";
+  console.log(name);
+  name = "hello";
+  console.log(name);
+  console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
+
+// var (don't ever use this!)
+// var hoisting (move declaration from bottom to top)
+// has no block scope
+{
+  age = 4;
+  var age;
+}
+console.log(age);
+
+// 3. Constants
+// favor immutable data type always for a few reasons:
+// - secutity
+// - thread safety
+// - reduce human mistakes
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// 4. Variable types
+// primitive, single item: number, string, boolean, null, underfiden, symbol
+// object, box container
+// function, first-class function
+const count = 17; // integer
+const size = 17.1; // declmal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+// number - special numeric vlaues: infinity, -infinity, NaN
+const infinity = 1 / 0;
+const nagativeInfinity = -1 / 0;
+const nAn = "not a number" / 2;
+console.log(infinity);
+console.log(nagativeInfinity);
+console.log(nAn);
+
+//string
+const char = "c";
+const brenden = "brenden";
+const greeting = "hello" + brenden;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brenden}!`; //template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true: any other value
+const canRead = true;
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`vlaue: ${x}, type: ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol("id");
+const symbol2 = Symbol("id");
+console.log(symbol1 === symbol2); // false
+
+const gSymbol1 = Symbol.for("id");
+const gSymbol2 = Symbol.for("id");
+console.log(gSymbol1 === gSymbol2); // true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
+
+// 5. Dynamic typing: dynamically typed language
+let text = "hello";
+console.log(text.charAt(0));
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = "7" + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = "8" / "2";
+console.log(`value: ${text}, type: ${typeof text}`);
+console.log(text.charAt(0));
