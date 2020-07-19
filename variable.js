aac0c11e7f5930b891abb1d757a74637402c454d
honggyu <zowoz0@naver.com>
2020-07-19 19:38:51 +0900

    operator, if, for loop

--- variable.js
+++ variable.js
@@ -4,7 +4,7 @@
 
 "use strict";
 
-// 2. Variable
+// 2. Variable, re(read/write)
 // let (added in ES6)
 let globalName = "global name";
 {
@@ -26,13 +26,19 @@ console.log(globalName);
 }
 console.log(age);
 
-// 3. Constants
+// 3. Constant, r(read only)
+// use const whenever possible.
+// only use let if variable needs to change.
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// Note!
+// immutable data types: primitive types, frozen objects (i.e. object.freeze())
+// Mutable data types: all objects by default are mutable in JS
 // favor immutable data type always for a few reasons:
 // - secutity
 // - thread safety
 // - reduce human mistakes
-const daysInWeek = 7;
-const maxNumber = 5;
 
 // 4. Variable types
 // primitive, single item: number, string, boolean, null, underfiden, symbol
