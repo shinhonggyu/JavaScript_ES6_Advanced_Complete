ebe3c6bbd8c8794cbc5902af72180cc92cc2267d
honggyu <zowoz0@naver.com>
2020-08-23 23:18:54 +0900

    async/callback.js

--- async/callback.js
+++ async/callback.js
@@ -2,14 +2,14 @@
 
 // JavaScript is synchronous.
 // Execute the code block in order after hoisting.
-// hoisting: var, function declaration
+// hoisting: var, function declaration 변수, 함수의선언은 호이스팅이됨
 console.log("1");
 setTimeout(() => console.log("2"), 1000);
 console.log("3");
 
 // Synchronous callback
-function printImmediately(value) {
-  value();
+function printImmediately(print) {
+  print();
 }
 printImmediately(() => console.log("hello"));
 
@@ -18,3 +18,54 @@ function printWithDelay(print, timeout) {
   setTimeout(print, timeout);
 }
 printWithDelay(() => console.log("async callback"), 2000);
+
+// Callback Hell example
+// 콜백은 다른 함수에 인수로 전달되는 함수⭐❗
+class UserStorage {
+  loginUser(id, password, onSuccess, onError) {
+    setTimeout(() => {
+      if (
+        (id === "ellie" && password === "dream") ||
+        (id === "coder" && password === "academy")
+      ) {
+        onSuccess(id);
+      } else {
+        onError(new Error("not found"));
+      }
+    }, 2000);
+  }
+
+  getRoles(user, onSuccess, onError) {
+    setTimeout(() => {
+      if (user === "ellie") {
+        onSuccess({ name: "ellie", role: "admin" });
+      } else onError(new Error("no access"));
+    }, 1000);
+  }
+}
+
+// 콜백은 다른 함수에 인수로 전달되는 함수⭐❗
+const userStorage = new UserStorage();
+const id = prompt("enter your id");
+const password = prompt("enter your password");
+userStorage.loginUser(
+  id,
+  password,
+  (user) => {
+    console.log(user); // ellie or coder
+    userStorage.getRoles(
+      user,
+      (userWithRole) => {
+        alert(
+          `Hello ${userWithRole.name}, you have a ${userWithRole.name} role`
+        );
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  },
+  (error) => {
+    console.log(error);
+  }
+);
