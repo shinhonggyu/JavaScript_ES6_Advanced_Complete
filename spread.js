5128bd1301f8a8f42fca76fa19f3c4e2a49426ac
shinhonggyu <zowoz0@naver.com>
2021-01-21 15:58:54 +0900

    Async JS Crush

--- spread.js
+++ spread.js
@@ -1,7 +1,7 @@
 {
   const obj1 = { key: 'key1' };
   const obj2 = { ...obj1, title: 'yo!' };
-  // console.log(obj3) => {key: "key1", title: "yo!"}
+  // console.log(obj2);
 }
 
 {
@@ -35,4 +35,43 @@
   const bugger = { bugger: '🍔' };
   const food = { ...pizza, ...bugger };
   // console.log(food);
+}
+
+{
+  const habit = { id: 1, name: 'Reading', count: 0 };
+  const habit2 = { id: 2, name: 'Coding', count: 0 };
+  const array = [habit, habit2];
+
+  const arrayCopy = [...array];
+  habit.name = 'Eating';
+
+  // console.log(arrayCopy)
+}
+
+{
+  const habit = { id: 1, name: 'Reading', count: 0 };
+  const habit2 = { id: 2, name: 'coding', count: 0 };
+  const array = [habit, habit2];
+
+  const arrayCopy = [...array];
+  // console.log(array, arrayCopy);
+
+  habit.name = 'Eating';
+}
+
+// card-maker
+{
+  const card = { name: 'shin', age: 30 };
+  const card2 = { ...card, ['name']: 'ggyu', ['sex']: 'male' };
+  console.log(card2);
+}
+
+{
+  const a = { id: '1', count: 0 }; // x123
+  const b = { id: '2', count: 0 }; // x234
+  const c = b; // 234
+  const d = { ...b }; // x456
+  b.count = 1;
+  //   console.log(c);
+  //   console.log(d);
 }
