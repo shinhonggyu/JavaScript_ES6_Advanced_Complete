f7e7938a56e62092a7a49ec5e91267db48071231
shinhonggyu <zowoz0@naver.com>
2021-02-05 23:07:34 +0900

    IF문 이해하기

---
+++ IF test/main.js
@@ -0,0 +1,57 @@
+// function IF() {
+//   const val = true;
+
+//   if (val) {
+//     return console.log(true);
+//   }
+
+//   console.log('null💖');
+// }
+
+// IF();
+// result: true
+
+// --------------------
+
+// function IF() {
+//   const val = true;
+
+//   if (val) {
+//     console.log(true);
+//   }
+
+//   console.log('null💖');
+// }
+
+// IF();
+// result: true
+//         null💖
+
+// -----------------
+
+// function IF() {
+//   const val = null;
+
+//   if (!val) {
+//     console.log(true);
+//   }
+
+//   console.log('null💖');
+// }
+
+// IF();
+
+// result: true
+//         null💖
+
+function IF() {
+  const userRef = null;
+
+  if (!userRef) {
+    console.log('test');
+  }
+
+  console.log('test2');
+}
+
+IF();
