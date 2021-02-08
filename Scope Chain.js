c360df2cfcadbd2c04b32fe83431bc8986f17fa9
shinhonggyu <zowoz0@naver.com>
2021-02-09 02:28:19 +0900

    Scope Chain

---
+++ Scope Chain.js
@@ -0,0 +1,31 @@
+// const x = 'x';
+
+// function findName() {
+//   const b = 'b';
+//   return printName();
+// }
+
+// function printName() {
+//   const c = 'c';
+//   return 'Shin';
+// }
+
+// function sayMyName() {
+//   const a = 'a';
+//   return findName();
+// }
+
+// console.log(sayMyName());
+
+function sayMyName() {
+  const a = 'a';
+  return function findName() {
+    const b = 'b';
+    return function printName() {
+      const c = 'c';
+      return 'Shin';
+    };
+  };
+}
+
+console.log(sayMyName()());
