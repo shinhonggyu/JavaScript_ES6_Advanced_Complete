dfbdc37d3f6dd88dcc74f7df746e2576344f092b
shinhonggyu <zowoz0@naver.com>
2021-04-23 23:59:39 +0900

    fix

--- Advanced Objects.js
+++ Advanced Objects.js
@@ -98,7 +98,7 @@ Quiz❗
 //#2 what is the value of property a for each object.
 const object1 = { a: 5 }; // 4
 const object2 = object1; // 4
-const object3 = object2; // 4
+const object3 = object2; // 
 const object4 = { a: 5}; // 5
 object1.a = 4;
 
